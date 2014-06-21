03474c01a4e7e1a68f9fa1aa4cbe208788e861f8
saud alsherie <saudalsherie@gmail.com>
2014-06-21 23:51:05 +0100

    same

---
+++ my/cow_class.py
@@ -0,0 +1,48 @@
+from animal_class import *
+
+class Cow(Animal):
+    """A generic Sheep"""
+
+    #constructor
+    def __init__(self,name):
+        #call parent class constructor with default values for potato
+        #growth rate = 1; food need = 3; water need = 6
+        super().__init__(name,1,3,6)
+        self._type = "Cow"
+
+    #overriding grow method for subclass
+    def grow(self,food,water):
+        if  food >= self._food_need and water >= self._water_need:
+            if self._status == "Young" and water > self._water_need:
+                self._weight += self._growth_rate * 1.5
+            elif self._status == "Calf" and water > self._water_need:
+                self._weight += self._growth_rate * 1.25
+            else:
+                self._weight = self._growth_rate
+        #increment the days growing
+        self._days_growing += 1
+        #update status
+        self._update_status()
+
+def get_name():
+    valid = False
+    while not valid:
+        name = input("Please enter a name for the cow: ")
+        if len(name) > 0:
+            valid = True
+        else:
+            print("Error! You need to enter a name for the cow")
+    return name
+
+def main():
+    name = get_name()
+    #create a new cow animal
+    new_cow = Cow(name)
+    #manually grow the animal
+    manual_grow(new_cow)
+    print(new_cow.report())
+    manual_grow(new_cow)
+    print(new_cow.report())
+
+if __name__ == "__main__":
+    main()
